cc6bbd9699ca10d944c384b14e6b848f46440cdb
jkomoefey23e@yahoo.co.uk <jkomoefey23e@yahoo.co.uk>
2024-02-19 15:58:33 +0100

    update on new flutter lessons

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  const MyApp({Key? key}): super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.green,
+        ),
+      ),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("FLutter Basics"),
+        ),
+        body:const Center(
+          child: Text("Hello  World"),
+        ),
+      ),
+    );
+  }
+}
