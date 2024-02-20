f8e96a955d887f1a083b5813a8473099a29f2ec6
jkomoefey23e@yahoo.co.uk <jkomoefey23e@yahoo.co.uk>
2024-02-20 19:22:25 +0100

    mobile lesson session @ Aptech Education Continue

--- lib/main.dart
+++ lib/main.dart
@@ -21,25 +21,48 @@ class MyApp extends StatelessWidget{
           title: const Text(
             "FLutter Basics",),
         ),
-        body: Center(
-          child: Container(
-            height: 200,
-            width: 200,
-            decoration: BoxDecoration(
-              color: Colors.blue,
-              borderRadius: BorderRadius.circular(100),
-              ),
-            child: Center(
-              child: const Text(
-                "Hello  World",
-                 style: TextStyle(
-                  color: Colors.red,
-                  fontSize: 20,
+        body: Container(
+            color: Colors.pink,
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Container(
+                  color: Colors.green,
+                  child: const Padding(
+                    padding: EdgeInsets.all(20.0),
+                    child: Text(
+                      'First Column child',
+                    ),
+                  ),
                 ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  height: 200,
+                  width: 200,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.blue,
+                  ),
+                  child: const Text(
+                    'Hello World',
+                    style: TextStyle(
+                      color: Colors.red,
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 40,
+                ),
+                const Text('Last Column child'),
+              ]
               ),
-            ),
           ),
-        ),
+      
         floatingActionButton: FloatingActionButton(
           onPressed: ()=> debugPrint('clicked'),
           child: const Icon(Icons.abc_outlined),
