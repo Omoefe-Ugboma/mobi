71bb0864507486f7f8f2d29bf8071eff9cec1959
jkomoefey23e@yahoo.co.uk <jkomoefey23e@yahoo.co.uk>
2024-02-21 15:45:15 +0100

    Update on lesson 5 A

---
+++ lib/main5.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  const MyApp({Key? key}): super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.green,
+        ),
+      ),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            "FLutter Basics",),
+        ),
+        body: Container(
+            color: Colors.pink,
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Container(
+                  color: Colors.green,
+                  child: const Padding(
+                    padding: EdgeInsets.all(20.0),
+                    child: Text(
+                      'First Column child',
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  height: 200,
+                  width: 200,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.blue,
+                  ),
+                  child: const Text(
+                    'Hello World',
+                    style: TextStyle(
+                      color: Colors.red,
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 40,
+                ),
+                const Text('Last Column child'),
+              ]
+              ),
+          ),
+      
+        floatingActionButton: FloatingActionButton(
+          onPressed: ()=> debugPrint('you clicked'),
+          child: const Icon(Icons.abc_outlined),
+        ),
+      ),
+    );
+  }
+}
